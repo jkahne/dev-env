8d7cf7b093d0958db305b7c4e6ac9e64f8302b0a
jkahne <jkahne@fastmail.com>
2025-04-30 14:32:36 -0400

    use harpoon over arrow

--- config/nvim/lua/homeless/lazy/harpoon.lua
+++ config/nvim/lua/homeless/lazy/harpoon.lua
@@ -1,97 +1,98 @@
 return {
-  {
-    "otavioschwanck/arrow.nvim",
-    -- dependencies = {
-    --   { "nvim-tree/nvim-web-devicons" },
-    -- or if using `mini.icons`
-    -- { "echasnovski/mini.icons" },
-    -- },
-    opts = {
-      show_icons = true,
-      leader_key = ";", -- Recommended to be a single key
-      buffer_leader_key = "m", -- Per Buffer Mappings
-      always_show_path = true,
-      mappings = {
-        edit = "e",
-        delete_mode = "d",
-        clear_all_items = "C",
-        toggle = "a",
-        open_vertical = "<C-v>",
-        open_horizontal = "<C-s>",
-        quit = "q",
-      },
-    },
-    -- config = function()
-    --   vim.keymap.set("n", "H", require("arrow.persist").previous)
-    --   vim.keymap.set("n", "L", require("arrow.persist").next)
-    --   vim.keymap.set("n", "<C-s>", require("arrow.persist").toggle)
-    -- end,
-  },
   -- {
-  -- 	"ThePrimeagen/harpoon",
-  -- 	branch = "harpoon2",
-  -- 	dependencies = { "nvim-lua/plenary.nvim" },
-  -- 	config = function()
-  -- 		local harpoon = require("harpoon")
-  --
-  -- 		-- REQUIRED
-  -- 		harpoon:setup()
-  -- 		-- REQUIRED
-  --
-  -- 		-- basic telescope configuration
-  -- 		local conf = require("telescope.config").values
-  -- 		local function toggle_telescope(harpoon_files)
-  -- 			local file_paths = {}
-  -- 			for _, item in ipairs(harpoon_files.items) do
-  -- 				table.insert(file_paths, item.value)
-  -- 			end
-  --
-  -- 			require("telescope.pickers")
-  -- 				.new({}, {
-  -- 					prompt_title = "Harpoon",
-  -- 					finder = require("telescope.finders").new_table({
-  -- 						results = file_paths,
-  -- 					}),
-  -- 					previewer = conf.file_previewer({}),
-  -- 					sorter = conf.generic_sorter({}),
-  -- 				})
-  -- 				:find()
-  -- 		end
-  --
-  -- 		vim.keymap.set("n", "<leader>m", function()
-  -- 			toggle_telescope(harpoon:list())
-  -- 		end, { desc = "Open harpoon window" })
-  --
-  -- 		vim.keymap.set("n", "<leader>ha", function()
-  -- 			harpoon:list():add()
-  -- 		end)
-  -- 		vim.keymap.set("n", "<leader>hx", function()
-  -- 			harpoon:list():remove()
-  -- 		end)
-  -- 		-- vim.keymap.set("n", "<leader>N", function()
-  -- 		--   harpoon.ui:toggle_quick_menu(harpoon:list())
-  -- 		-- end)
-  -- 		--
-  -- 		vim.keymap.set("n", "<leader>hh", function()
-  -- 			harpoon:list():select(1)
-  -- 		end)
-  -- 		vim.keymap.set("n", "<leader>jj", function()
-  -- 			harpoon:list():select(2)
-  -- 		end)
-  -- 		vim.keymap.set("n", "<leader>kk", function()
-  -- 			harpoon:list():select(3)
-  -- 		end)
-  -- 		vim.keymap.set("n", "<leader>mm", function()
-  -- 			harpoon:list():select(4)
-  -- 		end)
-  -- 		--
-  -- 		-- -- Toggle previous & next buffers stored within Harpoon list
-  -- 		vim.keymap.set("n", "<C-S-M>", function()
-  -- 			harpoon:list():prev()
-  -- 		end)
-  -- 		vim.keymap.set("n", "<C-S-N>", function()
-  -- 			harpoon:list():next()
-  -- 		end)
-  -- 	end,
+  --   "otavioschwanck/arrow.nvim",
+  --   -- dependencies = {
+  --   --   { "nvim-tree/nvim-web-devicons" },
+  --   -- or if using `mini.icons`
+  --   -- { "echasnovski/mini.icons" },
+  --   -- },
+  --   opts = {
+  --     show_icons = true,
+  --     leader_key = ";", -- Recommended to be a single key
+  --     buffer_leader_key = "m", -- Per Buffer Mappings
+  --     always_show_path = true,
+  --     mappings = {
+  --       edit = "e",
+  --       delete_mode = "d",
+  --       clear_all_items = "C",
+  --       toggle = "a",
+  --       open_vertical = "<C-v>",
+  --       open_horizontal = "<C-s>",
+  --       quit = "q",
+  --     },
+  --   },
+  --   -- config = function()
+  --   --   vim.keymap.set("n", "H", require("arrow.persist").previous)
+  --   --   vim.keymap.set("n", "L", require("arrow.persist").next)
+  --   --   vim.keymap.set("n", "<C-s>", require("arrow.persist").toggle)
+  --   -- end,
   -- },
+
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+
+      -- REQUIRED
+      harpoon:setup()
+      -- REQUIRED
+
+      -- basic telescope configuration
+      local conf = require("telescope.config").values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require("telescope.pickers")
+          .new({}, {
+            prompt_title = "Harpoon",
+            finder = require("telescope.finders").new_table({
+              results = file_paths,
+            }),
+            previewer = conf.file_previewer({}),
+            sorter = conf.generic_sorter({}),
+          })
+          :find()
+      end
+
+      vim.keymap.set("n", "<leader>m", function()
+        toggle_telescope(harpoon:list())
+      end, { desc = "Open harpoon window" })
+
+      vim.keymap.set("n", "<leader>ha", function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set("n", "<leader>hx", function()
+        harpoon:list():remove()
+      end)
+      -- vim.keymap.set("n", "<leader>N", function()
+      --   harpoon.ui:toggle_quick_menu(harpoon:list())
+      -- end)
+      --
+      vim.keymap.set("n", "<leader>hh", function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set("n", "<leader>jj", function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set("n", "<leader>kk", function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set("n", "<leader>mm", function()
+        harpoon:list():select(4)
+      end)
+      --
+      -- -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<C-S-M>", function()
+        harpoon:list():prev()
+      end)
+      vim.keymap.set("n", "<C-S-N>", function()
+        harpoon:list():next()
+      end)
+    end,
+  },
 }
