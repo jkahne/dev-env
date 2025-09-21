ff7b210139812dc195cf61ad4d85683de14d512a
jkahne <jkahne@fastmail.com>
2025-09-21 09:21:12 -0400

    nvim plugins

--- config/nvim/lua/jkahne/lazy/rest.lua
+++ config/nvim/lua/jkahne/lazy/rest.lua
@@ -1,6 +1,7 @@
 return {
   {
     "mistweaverco/kulala.nvim",
+    enabled = false,
     keys = {
       -- { "<leader>Rs", desc = "Send request" },
       -- { "<leader>Ra", desc = "Send all requests" },
